afaa8a0d4ce8876acd237dd3040202f933a5d7a2
Adam Bartholomew <adam.bartholomew@gordon.edu>
2017-09-11 12:13:47 -0400

    remove API credentials

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-config = {  "api_key" : "TZCMV9KcMARaudSsPx26mkaRC",
-			"api_secret" : "yANbzzGzb36weJlrZviF44l59QKax0tEJu3cyigb8Se7AlW3c7",
-			"access_token" : "903655406345564161-ldk527LYvZbcJlLamgWCZdVOPiHfNg5",
-			"access_secret" : "ARAlX9fV0FsPvWg0qIZc1Rg9zJAm2M62lUHfTyDYhGBpM",
-			"sleep_for" : "3600", # 1 hour
+config = {  "api_key" : "",
+			"api_secret" : "",
+			"access_token" : "",
+			"access_secret" : "",
+			"sleep_for" : "10800", # 3 hours
 			"comment_char" : "#",
 			"double_line_char" : "&",
 			"dictionary_path" : "./dict/sayings.txt",		
