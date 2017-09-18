ba92cd0f378733883dd55cc61a802a90f5990350
Adam Bartholomew <adam.bartholomew17@gmail.com>
2017-09-18 13:48:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-config = {  "api_key" : "TZCMV9KcMARaudSsPx26mkaRC",
-			"api_secret" : "yANbzzGzb36weJlrZviF44l59QKax0tEJu3cyigb8Se7AlW3c7",
-			"access_token" : "903655406345564161-ldk527LYvZbcJlLamgWCZdVOPiHfNg5",
-			"access_secret" : "ARAlX9fV0FsPvWg0qIZc1Rg9zJAm2M62lUHfTyDYhGBpM",
+config = {  "api_key" : "",
+			"api_secret" : "",
+			"access_token" : "",
+			"access_secret" : "",
 			"sleep_for" : "10800", # 3 hours
 			"comment_char" : "#",
 			"double_line_char" : "&",
