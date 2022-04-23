101349dc1cc003e128ff765bc847b7e32812c21f
adam-bartholomew <adam.bartholomew17@gmail.com>
2022-04-23 16:06:41 -0400

    1. Update how the logging is handled and displayed through basic config. 2. Update the check followers function. 3. Reformat the main function to try/except/else correctly.

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ config = {"consumer_key": "",
           "consumer_secret": "",
           "access_token": "",
           "access_secret": "",
-          "sleep_for": "86400",  # in seconds: 24 hours
+          "sleep_for": "880000",  # in seconds: 24 hours
           "comment_char": "#",
           "double_line_char": "&",
-          "dictionary_path": "./quoteDict.py",
+          "log_format": "[%(asctime)s] %(levelname)s:%(message)s",
+          "logging_date_format": "%m/%d/%Y %I:%M:%S %p",
+          "base_log_dir": "./logs/",
+          "base_log_extension": ".log",
           }
--- office-bot.py
+++ office-bot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 
 # A Twitter bot that tweets random quotes from "The Office".
 # Created by Adam Bartholomew.
@@ -15,7 +15,7 @@ import random
 from config import config
 import lib.quoteDict as quoteDict
 
-# Get information from config file.
+# Get config info.
 CONSUMER_KEY = config["consumer_key"]
 CONSUMER_SECRET = config["consumer_secret"]
 ACCESS_TOKEN = config["access_token"]
@@ -23,8 +23,14 @@ ACCESS_SECRET = config["access_secret"]
 COMMENT_CHAR = config["comment_char"]
 DOUBLE_LINE_CHAR = config["double_line_char"]
 SLEEP_FOR = int(config["sleep_for"])
+DATE_FORMAT = config["logging_date_format"]
+LOG_FORMAT = config["log_format"]
+BASE_LOG_DIR = config["base_log_dir"]
+BASE_LOG_EXT = config["base_log_extension"]
 
-# Create global variables.
+# Create variables.
+log_filename = BASE_LOG_DIR + "twitter-bot_" + datetime.datetime.now().strftime("%Y%m%d") + BASE_LOG_EXT
+logging.basicConfig(filename=log_filename, format=LOG_FORMAT, level=logging.DEBUG, datefmt=DATE_FORMAT)
 log = logging.getLogger()
 sleep_time = datetime.timedelta(seconds=int(SLEEP_FOR))
 used_quotes = {}
@@ -32,7 +38,7 @@ used_quotes = {}
 
 # Setup connection with Twitter.
 def connect():
-    log.info("Connecting to Twitter...")
+    log.info("Attempting to connect to Twitter.")
     auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
     return tweepy.API(auth)
 
@@ -40,15 +46,15 @@ def connect():
 # Sends a tweet given the line and connection to the api.
 #       quote - what to tweet.
 #       conn - api connection
-def send_tweet(quote, conn, date):
+def send_tweet(quote, conn):
     if quote != "":
         try:
             # conn.update_status(status=quote)
-            log.info("[%s]: Tweet sent with the quote %s", date, quote)
-            print("Tweet sent at %s. [%s]" % (date, quote))
+            log.info("Tweet sent: %s", quote)
+            print("Tweet sent: %s" % quote)
             return True
         except tweepy.TweepyException as err:
-            log.error("[%s]: Error occurred sending tweet: %s", date, err)
+            log.error("Error occurred sending tweet: %s", err)
             return False
 
 
@@ -60,8 +66,8 @@ def get_quote():
     index = random.randint(0, len(quoteDict.quotes))
     quote = list(quoteDict.quotes.keys())[index]
     get_least_used_quotes()
-    #quote = random.choice(get_least_used_quotes())
-    print("Selected: " + quote)
+    # quote = random.choice(get_least_used_quotes())
+    print("Quote Selected: %s" % quote)
 
     while is_used(quote):
         index = random.randint(0, len(quoteDict.quotes))
@@ -71,7 +77,7 @@ def get_quote():
     quoteDict.quotes[quote]['used'] = 1
 
     msg = "\"%s\" - %s" % (quote, info.get('source'))
-    log.info("Quote selected: [" + quote + "]\n" + str(quoteDict.quotes[quote]))
+    log.info("Quote selected: %s\n  %s", quote,  str(quoteDict.quotes[quote]))
 
     return msg
 
@@ -81,7 +87,8 @@ def is_used(quote):
     return bool(list_of_values[1])
 
 
-def get_most_used(): #todo: set all of the quotes to be unused once they all have been used th same # of times. No 1 quote can be used a 2nd time before they all have been used once.
+def get_most_used():
+    # todo: set all of the quotes to be unused once they all have been used th same # of times. No 1 quote can be used a 2nd time before they all have been used once.
     print('get most used quote')
 
 
@@ -90,7 +97,7 @@ def get_least_used_quotes():
     least_used = list()
 
     least_used_dict = quoteDict.quotes
-    print(least_used_dict)
+    # print(least_used_dict)
 
     for quote in quoteDict.quotes:
         # print(quote['used'])
@@ -104,58 +111,49 @@ def get_least_used_quotes():
 
 
 # Follows someone back if they follow this account.
-def check_followers(conn, date):
-    log.info("[%s]: Checking followers...", date)
-    bot = conn.me()
-    log.info(conn.followers())
-    for follower in conn.followers():
+def check_followers(conn):
+    log.info("Checking %s's followers", conn.verify_credentials().screen_name)
+    for follower in conn.get_followers():
         if not follower.following:
             try:
-                conn.create_friendship(follower.id, follower.screen_name)
+                # conn.create_friendship(follower.screen_name, follower.id)
                 log.info("ALERT: Now following %s!", follower.screen_name)
-            except:
-                log.info("[%s]: An unknown exception occurred while following", date)
-        else:
-            log.info("All followers are followed.")
-    log.info("[%s]: Follower check COMPLETE.", date)
+            except tweepy.TweepyException as err:
+                log.info("An unknown exception occurred while following: %s", err)
+
+    log.info("All followers are now followed.")
+    log.info("Follower check COMPLETE.")
 
 
 # Manages the process of sending a tweet.
-def iteration(conn, date):
-    log.info("[%s] Start of a new iteration.", date)
-
-    log.info("Sending tweet...")
-    if send_tweet(get_quote(), conn, date) is True:
+def iteration(conn):
+    log.info("Start of a new iteration.")
+    if send_tweet(get_quote(), conn) is True:
         return True
     else:
-        log.error("[%s]: Caught a tweepy error, trying again...", date)
-        print("[%s]: Caught a tweepy error, trying again...", date)
+        log.error("Caught a tweepy error, trying again...")
+        print("Caught a tweepy error, trying again...")
         return False
 
 
 # Main function
 def main():
     quoteDict.get_least_used()
-    base_dir = "./logs/"
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logfile = base_dir + "twitter-bot_" + datetime.datetime.now().strftime("%Y%m%d") + ".log"
-    logging.basicConfig(filename=logfile, level=logging.INFO)
-    log.info("Starting up for the first time...")
+    log.info("Starting up for the first time")
 
     try:
         conn = connect()
-        log.info("Connected...")
     except tweepy.TweepyException as err:
-        log.warning("Connection could not be made to Twitter:\n - %s", err)
+        log.warning("Connection could not be made to Twitter: - %s", err)
     else:
-        log.warning("[%s]: Connection could not be made to Twitter", date)
+        log.info("Connection made successfully")
 
     # Sends a new tweet and checks for new followers, sleeps.
     while conn:
-        # check_followers(conn, date)
-        did_tweet = iteration(conn, date)
+        check_followers(conn)
+        did_tweet = iteration(conn)
         if did_tweet is False:
-            did_tweet = iteration(conn, date)
+            did_tweet = iteration(conn)
         else:
             log.info("Going to sleep for %s ", sleep_time)
             print("Sleeping...")
